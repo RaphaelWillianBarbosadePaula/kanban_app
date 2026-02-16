b252e47ae2c7847a367a8fa1cadc3ff8db7ccfc8
Raphael Willian <raphaelwillian.bdp44@gmail.com>
2026-02-16 17:36:27 -0300

    feat: add home page and tailwind configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   resources :users
-  get "up" => "rails/health#show", as: :rails_health_check
+
+  root "home#index"
+
+  # Painel do Sidekiq
+  mount Sidekiq::Web => "/sidekiq"
 end
