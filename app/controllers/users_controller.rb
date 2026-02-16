035e0b7ca5b86a4198843b4b50d556f38f9b9a74
Davi Santos <davisantosp.dev@gmail.com>
2026-02-16 15:20:33 -0300

    fix: fixing in code review on User CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,70 +1,51 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  skip_before_action :verify_authenticity_token
 
-  # GET /users or /users.json
+  before_action :set_user, only: %i[ show edit update destroy ] 
+
   def index
     @users = User.all
+    render json: @users, except: [ :password_digest ]
   end
 
-  # GET /users/1 or /users/1.json
   def show
+    render json: @user, except: [ :password_digest ]
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users or /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
+      if @user.save 
+        render json: @user, status: :created, location: @user, except: [ :password_digest] 
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render json: {errors: @user.errors}, status: :unprocessable_entity
       end
-    end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: "User was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      render json: @user, status: :ok, except: [ :password_digest] 
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to users_path, notice: "User was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params.expect(:id))
+      @user = User.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def user_params
-      params.expect(user: [ :name, :email, :password, :password_confirmation ])
+      params.require(:user).permit( :name, :email, :password, :password_confirmation )
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
+    normalizes :email, with: ->(e) {e.strip.downcase}
+
+    validates :name, presence: true, length: {maximum: 150}
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
 end
