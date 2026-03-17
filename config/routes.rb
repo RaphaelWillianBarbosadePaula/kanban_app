0b340763c3db0ce3b8c8a3f9a791d608c2b79f8e
Davi Santos <davisantosp.dev@gmail.com>
2026-03-17 16:07:15 -0300

    feat: Invitation by email implementation through mailer

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :projects do
     get "index_members", on: :member
   end
+  resources :invitations do
+    get "accept_invitation", on: :collection, as: :accept
+  end
 
   get "/signup", to: "users#new", as: :signup
   get "/login", to: "authentication#new", as: :login
@@ -13,4 +16,7 @@ Rails.application.routes.draw do
 
   root "home#index"
   mount Sidekiq::Web => "/sidekiq"
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
